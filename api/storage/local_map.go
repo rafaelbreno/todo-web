66515e4259f27f74a9a37d37d819c515525fc021
Rafael <rafael_breno@outlook.com>
2022-08-17 00:14:31 -0300

    fix: Switch to Pointer binds

--- api/storage/local_map.go
+++ api/storage/local_map.go
@@ -14,15 +14,15 @@ type LocalMap struct {
 }
 
 // NewLocalMap returns a new instace of LocalMap.
-func NewLocalMap() LocalMap {
-	return LocalMap{
+func NewLocalMap() *LocalMap {
+	return &LocalMap{
 		lists: make(map[string]models.List),
 		items: make(map[string]models.Item),
 	}
 }
 
 // Create inserts a model instance to the local DB.
-func (l LocalMap) Create(v any) error {
+func (l *LocalMap) Create(v any) error {
 	switch m := v.(type) {
 	case models.Item:
 		l.items[m.ID.String()] = m
@@ -36,7 +36,7 @@ func (l LocalMap) Create(v any) error {
 }
 
 // Delete deletes an existent model instance in the local DB.
-func (l LocalMap) Delete(v any) error {
+func (l *LocalMap) Delete(v any) error {
 	switch m := v.(type) {
 	case models.Item:
 		if _, ok := l.items[m.ID.String()]; !ok {
@@ -56,19 +56,19 @@ func (l LocalMap) Delete(v any) error {
 }
 
 // Update updates an existent model instance in the local DB.
-func (l LocalMap) Read(v any) error {
+func (l *LocalMap) Read(v any) error {
 	switch m := v.(type) {
-	case models.Item:
+	case *models.Item:
 		if _, ok := l.items[m.ID.String()]; !ok {
 			return fmt.Errorf("item with id '%s' does not exists", m.ID.String())
 		}
-		v = l.items[m.ID.String()]
+		*m = l.items[m.ID.String()]
 		return nil
-	case models.List:
+	case *models.List:
 		if _, ok := l.lists[m.ID.String()]; !ok {
 			return fmt.Errorf("list with id '%s' does not exists", m.ID.String())
 		}
-		v = l.lists[m.ID.String()]
+		*m = l.lists[m.ID.String()]
 		return nil
 	default:
 		return fmt.Errorf("type '%T' not supported", v)
@@ -76,17 +76,17 @@ func (l LocalMap) Read(v any) error {
 }
 
 // ReadAll retrieves all values from the DB of a given Model.
-func (l LocalMap) ReadAll(v any) error {
+func (l *LocalMap) ReadAll(v any) error {
 	switch m := v.(type) {
-	case []models.Item:
+	case *[]models.Item:
 		for _, v := range l.items {
-			m = append(m, v)
+			*m = append(*m, v)
 		}
 
 		return nil
-	case []models.List:
+	case *[]models.List:
 		for _, v := range l.lists {
-			m = append(m, v)
+			*m = append(*m, v)
 		}
 
 		return nil
@@ -96,7 +96,7 @@ func (l LocalMap) ReadAll(v any) error {
 }
 
 // Update updates an existent model instance in the local DB.
-func (l LocalMap) Update(v any) error {
+func (l *LocalMap) Update(v any) error {
 	switch m := v.(type) {
 	case models.Item:
 		if _, ok := l.items[m.ID.String()]; !ok {
@@ -105,7 +105,7 @@ func (l LocalMap) Update(v any) error {
 		l.items[m.ID.String()] = m
 		return nil
 	case models.List:
-		if _, ok := l.items[m.ID.String()]; !ok {
+		if _, ok := l.lists[m.ID.String()]; !ok {
 			return fmt.Errorf("list with id '%s' does not exists", m.ID.String())
 		}
 		l.lists[m.ID.String()] = m
