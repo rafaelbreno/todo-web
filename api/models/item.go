7c51411289b5574c197d6c210c433c5b3b99063e
Rafael <rafael_breno@outlook.com>
2022-08-16 21:42:02 -0300

    feat: Add Timestamp field in Item and List models

--- api/models/item.go
+++ api/models/item.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"github.com/rafaelbreno/todo-web/api/enum"
 )
@@ -8,8 +10,11 @@ import (
 // Item - A data model which represents
 // 	each item of a list.
 type Item struct {
-	ID     uuid.UUID       `json:"id"`
-	ListID uuid.UUID       `json:"list_id"`
-	Status enum.ItemStatus `json:"status"`
-	Text   string          `json:"text"`
+	ID        uuid.UUID       `json:"id"`
+	ListID    uuid.UUID       `json:"list_id"`
+	Status    enum.ItemStatus `json:"status"`
+	Text      string          `json:"text"`
+	CreatedAt time.Time       `json:"created_at"`
+	UpdatedAt time.Time       `json:"updated_at"`
+	DeletedAt time.Time       `json:"deleted_at"`
 }
--- api/models/list.go
+++ api/models/list.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"github.com/rafaelbreno/todo-web/api/enum"
 )
@@ -8,7 +10,10 @@ import (
 // List - A data model which is used for
 // 	declaring lists and connecting it's items.
 type List struct {
-	ID     uuid.UUID       `json:"id"`
-	Name   string          `json:"name"`
-	Status enum.ListStatus `json:"status"`
+	ID        uuid.UUID       `json:"id"`
+	Name      string          `json:"name"`
+	Status    enum.ListStatus `json:"status"`
+	CreatedAt time.Time       `json:"created_at"`
+	UpdatedAt time.Time       `json:"updated_at"`
+	DeletedAt time.Time       `json:"deleted_at"`
 }
