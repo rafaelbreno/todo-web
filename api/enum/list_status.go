7194d509fb2b0d4a0458c1d25aa5d64ee30ff32d
Rafael <rafael_breno@outlook.com>
2022-08-16 21:36:28 -0300

    feat: Add ListStatus enums

---
+++ api/enum/list_status.go
@@ -0,0 +1,26 @@
+package enum
+
+// ListStatus represents the status of an list.
+type ListStatus int8
+
+const (
+	// ListNotStarted is when the list still haven't been started yet.
+	ListNotStarted ListStatus = iota
+	// ListInProgress is when the list still in InProgress
+	ListInProgress
+	// ListCompleted is when the list was completed.
+	ListCompleted
+	// ListCanceled is when the list was canceled.
+	ListCanceled
+)
+
+var (
+	// ListStatusMap is a map to get the string value
+	// of an ListStatus.
+	ListStatusMap = map[ListStatus]string{
+		ListNotStarted: "not_started",
+		ListInProgress: "in_progress",
+		ListCompleted:  "completed",
+		ListCanceled:   "canceled",
+	}
+)
