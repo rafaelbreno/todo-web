436579419144cb0a2d7e93bb73e116dc6b4a2e7f
Rafael <rafael_breno@outlook.com>
2022-08-16 22:54:41 -0300

    fix: Update Storage interface methods

--- api/storage/storage.go
+++ api/storage/storage.go
@@ -2,12 +2,12 @@ package storage
 
 // Storage wraps all actions related to storage.
 type Storage interface {
-	Create(any) (any, error)
-	Delete(string, any) error
+	Create(any) error
+	Delete(any) error
 	HealthCheck() error
 	Migrate(...any) error
 	Open() error
-	Read(string, any) (any, error)
-	ReadAll(any) (any, error)
-	Update(string, any) (any, error)
+	Read(any) error
+	ReadAll(any) error
+	Update(any) error
 }
