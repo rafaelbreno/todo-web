9f10629bfa69eaa2d73636c08e2db1746bc7c1e9
Rafael <rafael_breno@outlook.com>
2022-08-17 00:32:17 -0300

    feat: Implement HTTP Server in main

--- api/main.go
+++ api/main.go
@@ -1,6 +1,12 @@
 package main
 
-import "go.uber.org/zap"
+import (
+	"os"
+	"os/signal"
+
+	"github.com/rafaelbreno/todo-web/api/server"
+	"go.uber.org/zap"
+)
 
 func main() {
 	logger, err := zap.NewProduction()
@@ -12,4 +18,17 @@ func main() {
 	undo := zap.ReplaceGlobals(logger)
 
 	defer undo()
+
+	srv := server.NewHTTP()
+
+	go srv.App.Listen(":5001")
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	<-c
+	zap.L().Info("gracefully shutdown...")
+	if err := srv.App.Shutdown(); err != nil {
+		zap.Error(err)
+	}
 }
