560e2a8b5b2201ef2b60b537728a53432a8e1b7f
Rafael <rafael_breno@outlook.com>
2022-08-17 22:53:09 -0300

    feat: Add Storage parameter for HealthCheck handler

--- api/handler/health_check.go
+++ api/handler/health_check.go
@@ -1,13 +1,23 @@
 package handler
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/rafaelbreno/todo-web/api/storage"
+)
 
 // HealthCheck returns a Fiber Handler to check the
 // 	health of the APP.
 // TODO:
 // 	- Implement Storage Health Check
-func HealthCheck() (string, func(c *fiber.Ctx) error) {
+func HealthCheck(st storage.Storage) (string, func(c *fiber.Ctx) error) {
 	return "/health", func(c *fiber.Ctx) error {
+		if err := st.HealthCheck(); err != nil {
+			return c.
+				Status(fiber.StatusInternalServerError).
+				JSON(fiber.Map{
+					"message": err.Error(),
+				})
+		}
 		return c.JSON(fiber.Map{
 			"message": "healthy",
 		})
