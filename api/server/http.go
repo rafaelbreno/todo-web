127a8eabff03526fac75aae2d8d026177d86ebf8
Rafael <rafael_breno@outlook.com>
2022-08-17 21:17:34 -0300

    feat: Now HTTP Receives Storage instance

--- api/server/http.go
+++ api/server/http.go
@@ -1,8 +1,12 @@
 package server
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/rafaelbreno/todo-web/api/handler"
+	"github.com/rafaelbreno/todo-web/api/storage"
 )
 
 // HTTP - manages the HTTP server.
@@ -11,16 +15,23 @@ type HTTP struct {
 }
 
 // NewHTTP returns a new instance of HTTP.
-func NewHTTP() *HTTP {
+func NewHTTP(st storage.Storage) *HTTP {
 	h := &HTTP{
-		App: fiber.New(),
+		App: fiber.New(fiber.Config{
+			StrictRouting: true,
+		}),
 	}
 
-	h.setHandlers()
+	h.
+		setDefaultHandlers()
+
+	handler.SetListHandlers(h.App, st)
+
+	data, _ := json.MarshalIndent(h.App.Stack(), "", "  ")
+
+	fmt.Println(string(data))
 
 	return h
 }
 
-func (h *HTTP) setHandlers() {
-	h.App.Get(handler.HealthCheck())
-}
+func (h *HTTP) setDefaultHandlers() { h.App.Get(handler.HealthCheck()) }
